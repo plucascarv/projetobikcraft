12234779f6c722da5f12c841a79da7b855b8b15b
Pedro Carvalho <plucas682@gmail.com>
2025-04-01 15:27:45 -0300

    interação-pag-bike-interna-com-pag-orçamento

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
+// Menu Ativo
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
     if (location.href.includes(link.href)) {
-        link.classList.add('ativo')
+        link.classList.add('ativo');
     }
 }
 
 links.forEach(ativarLink);
+
+// Ativar Itens Orçamento
+const params = new URLSearchParams(location.search);
+
+function ativarProduto(param) {
+    const element = document.getElementById(param);
+    if (element) {
+    element.checked = true;
+    }
+}
+
+params.forEach(ativarProduto);
